d025ecce401da024843b2f4cd9b5fe4f400fbbbb
Logical Contradiction <chrisgiaramita@yahoo.com>
2022-02-28 19:44:15 -0500

    Created Card object to hold needed data and tests to support it.

--- HotCCSVGenerator/HotCCSVGenerator.py
+++ HotCCSVGenerator/HotCCSVGenerator.py
@@ -2,8 +2,47 @@ from . import config
 from bs4 import BeautifulSoup
 import re
 import requests
+from typing import TypedDict
 
 
+class Card(TypedDict):
+    name: str
+    translatedName: str
+    cardNum: str
+    rarity: str
+    color: str
+    side: str
+    type: str
+    level: int
+    cost: int
+    power: int
+    soul: int
+    traits: str
+    triggers: str
+    flavor: str
+    text: str
+    
+    def __eq__(self, other:object) -> bool:
+        if isinstance(other, Card):
+            return self.name == other.name \
+            and self.translatedName == other.translatedName \
+            and self.cardNum == other.cardNum \
+            and self.rarity == other.rarity \
+            and self.color == other.color \
+            and self.side == other.side \
+            and self.character == other.character \
+            and self.level == other.level \
+            and self.cost == other.cost \
+            and self.power == other.power \
+            and self.soul == other.soul \
+            and self.traits == other.traits \
+            and self.triggers == other.triggers \
+            and self.flavor == other.flavor \
+            and self.text == other.text
+        return False
+        
+
+      
 def makeSoupFromFile(filepath):
     with open(filepath, "r", encoding=config.FILE_ENCODING) as file:
         soup = BeautifulSoup(file, "html.parser")
@@ -15,4 +54,4 @@ def makeSoupFromWebpage(url):
 
 
 if __name__ == "__main__":
-    pass
+    pass 
--- tests/HotCCSVGenerator_tests.py
+++ tests/HotCCSVGenerator_tests.py
@@ -2,9 +2,52 @@ import unittest
 
 from hotCCSVGenerator import hotCCSVGenerator
 
-class HotCCSVGeneratorTest(unittest.TestCase):
-    pass
+data = {"name": "ボブ",
+        "translatedName": "Bob",
+        "cardNum": "AA/Z00-069 ZZ",
+        "rarity": "ZZ",
+        "color": "purple",
+        "side": "W",
+        "type": "character",
+        "level": 42,
+        "cost": 500,
+        "power": 0,
+        "soul": 22,
+        "traits": "magic (mag), wisdom (wis)",
+        "triggers": "soul",
+        "flavor": "flavor text goes here",
+        "text": "When that happens, do the other thing."
+        }
 
+class HotCCSVGeneratorTest(unittest.TestCase):
+    
+    def test_Card_Object_Creation(self):
+        global data
+        card = hotCCSVGenerator.Card(data)
+        
+        self.assertEqual(card["name"],data["name"])
+        self.assertEqual(card["translatedName"],data["translatedName"])
+        self.assertEqual(card["cardNum"],data["cardNum"])
+        self.assertEqual(card["rarity"],data["rarity"])
+        self.assertEqual(card["color"],data["color"])
+        self.assertEqual(card["side"],data["side"])
+        self.assertEqual(card["type"],data["type"])
+        self.assertEqual(card["level"],data["level"])
+        self.assertEqual(card["cost"],data["cost"])
+        self.assertEqual(card["power"],data["power"])
+        self.assertEqual(card["soul"],data["soul"])
+        self.assertEqual(card["traits"],data["traits"])
+        self.assertEqual(card["triggers"],data["triggers"])
+        self.assertEqual(card["flavor"],data["flavor"])
+        self.assertEqual(card["text"],data["text"])
+        
+    def test_Card_Object_eq(self):
+        global data
+        card1 = hotCCSVGenerator.Card(data)
+        card2 = hotCCSVGenerator.Card(data)
+        self.assertEqual(card1, card2, "Made with the same data, should be the same.")
+        
+    
 
 if __name__ == "__main__":
     unittest.main()
